39c89df162bff83c88dab6ee254c614cd8b5bbe5
stshkmr2100 <satish.gupta@girnarsoft.com>
2019-11-02 12:03:42 +0530

    changes

--- src/app/detail-page/detail-page.component.ts
+++ src/app/detail-page/detail-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { ActivatedRoute } from '@angular/router';
@@ -13,13 +13,30 @@ export class DetailPageComponent implements OnInit {
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
   public ApplicationData;
   public showLoaded = false;
+  public showImage = false;
+  public showimageUrl = '';
 
   getAppDetails(appId): void {
     this.showLoaded = true;
     this.http.get<any>(`${environment.apiUrl}/web/get-detailed-data?id=${appId}`).subscribe(data => {
       this.ApplicationData = data.data;
+      //console.log(this.ApplicationData);
       this.showLoaded = false;
     });
+  }
+  public scrollLeft(el: HTMLElement): void {
+    el.scrollLeft -= 20;
+  }
+
+  public scrollRight(el: HTMLElement): void {
+    el.scrollLeft += 20;
+  }
+  public showImageDiv(url: any): void {
+   this.showimageUrl = url;
+   this.showImage = true;
+  }
+  public closeImageShow(): void {
+    this.showImage = false;
   }
 
   ngOnInit() {
